4830d3c66e1c9f7212ade0426fb310366b4524ac
Srikanth <vtu26029@veltech.edu.in>
2026-08-18 22:43:14 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.75%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    static int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int val = sc.nextInt();
+
+        int k = removeElement(nums, val);
+
+        System.out.println(k);
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
